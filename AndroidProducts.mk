45f61d22f29a4002bbfd74bfb1b6630c97332b7e
hmtheboy154 <buingoc67@gmail.com>
2024-05-01 16:50:41 +0000

    rpi5: Adding LMODroid target
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,7 +7,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_rpi5.mk \
     $(LOCAL_DIR)/aosp_rpi5_car.mk \
-    $(LOCAL_DIR)/aosp_rpi5_tv.mk
+    $(LOCAL_DIR)/aosp_rpi5_tv.mk \
+    $(LOCAL_DIR)/lmodroid_rpi5.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_rpi5-trunk_staging-userdebug \
